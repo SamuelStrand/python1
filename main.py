029f7e6f97d467ee27c39065e2cdc916ba636677
SamuelStrand <105794723+SamuelStrand@users.noreply.github.com>
2022-05-18 19:51:46 +0600

    Наш первый commit

---
+++ main.py
@@ -0,0 +1,3 @@
+var_bool_1 = True
+var_bool_1 = type(type(var_bool_1))
+print((type)var_bool_1)
---
+++ timer.py
@@ -0,0 +1,22 @@
+
+import time
+
+hours = 0
+minutes = 0
+seconds = 0
+
+while True:
+    seconds += 1
+    if seconds > 59:
+        minutes += 1
+        seconds = 0
+    if minutes > 59:
+        hours += 1
+        minutes = 0
+        seconds = 0
+    if hours > 23:
+        hours = 0
+        minutes = 0
+        seconds = 0
+    time.sleep(1)
+    print(f"{hours}:{minutes}:{seconds}")
