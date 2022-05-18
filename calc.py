b43c52d413ddf9eab7020aa89ac1728eaa8b0130
SamuelStrand <105794723+SamuelStrand@users.noreply.github.com>
2022-05-18 21:18:58 +0600

    calc update

--- calc.py
+++ calc.py
@@ -4,12 +4,56 @@ def calc(number1, number2, oper):
         return number1 + number2
     if oper == "-":
         return number1 - number2
+    if oper == "*":
+        return number1 * number2
+    if oper == "/":
+        if number2 != 0:
+            return number1 / number2
+        else:
+            print("Вы ввели ноль")
+    if oper == "**":
+        return number1 ** number2
+    if oper == "//":
+        if number2 != 0:
+            return number1 // number2
+        else:
+            print("Вы ввели ноль")
+            return (print("Введите другое число"))
+    import math
+    if oper == "math.sqrt":
+        return math.sqrt(number1), math.sqrt(number2)
+    if oper == "%":
+        return number1 % number2
+
+
 
 
 result = calc(10, 3, "+")
 print(result)
-result = calc(-11, -5, "+")
-print(result)
+
 result = calc(10, 3, "-")
 print(result)
+
+result = calc(10, 3, "*")
+print(result)
+
+result = calc(10, 0, "/")
+print(result)
+
+result = calc(10, 3, "**")
+print(result)
+
+result = calc(10, 3, "//")
+print(result)
+
+result = calc(64, 144, "math.sqrt")
+print(result)
+
+result = calc(10, 4, "%")
+print(result)
+
+
+
+
+
 
